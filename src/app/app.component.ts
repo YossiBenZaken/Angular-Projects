ba148b424792cee338c6cdb3a8d3f4695d9ce7b9
Yossi Ben Zaken <yossibz@gmail.com>
2020-08-26 11:32:49 +0300

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  choices: Array<string> = ['Rock', 'Paper', 'scissor'];
+  you: string;
+  computer: string;
+  show = false;
+  result: string;
   title = 'RockPaperScissor';
+  choice(num: number) {
+    this.show = true;
+    this.you = this.choices[num];
+    this.computer = this.choices[Math.round(Math.random() * 2)];
+    if (this.you === this.computer) {
+      this.result = 'Draw';
+    } else if (
+      this.you === this.choices[0] && this.computer === this.choices[1] ||
+      this.you === this.choices[1] && this.computer === this.choices[2] ||
+      this.you === this.choices[2] && this.computer === this.choices[0]) {
+      this.result = 'You Lose';
+    } else {
+      this.result = 'You Win';
+    }
+  }
 }
