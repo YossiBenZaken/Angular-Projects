06f48f686202f9ab9f286e9f0fc25ad8b905e0d2
Yossi Ben Zaken <yossibz@gmail.com>
2020-08-27 20:36:47 +0300

    add animations to notes project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NoteHomeComponent } from './notes/note-home/note-home.component';
 import { MainLayoutComponent } from './notes/main-layout/main-layout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -18,7 +19,7 @@ import { Game1Component } from './game1/game1.component';
     NoteCardComponent,
     NoteDetailsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/notes/note-home/note-home.component.ts
+++ src/app/notes/note-home/note-home.component.ts
@@ -1,11 +1,93 @@
 import { NotesService } from './../shared/notes.service';
 import { Note } from './../shared/note.model';
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-note-home',
   templateUrl: './note-home.component.html',
   styleUrls: ['./note-home.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0
+        }),
+        animate(
+          '50ms',
+          style({
+            height: '*',
+            'margin-bottom': '*',
+            paddingTop: '*',
+            paddingBottom: '*',
+            paddingRight: '*',
+            paddingLeft: '*'
+          })
+        ),
+        animate(68)
+      ]),
+      transition('* => void', [
+        animate(50, style({ transform: 'scale(1.05)' })),
+        animate(
+          50,
+          style({
+            transform: 'scale(1)',
+            opacity: 0.75
+          })
+        ),
+        animate(
+          '120ms ease-out',
+          style({
+            transform: 'scale(0.68)',
+            opacity: 0
+          })
+        ),
+        animate(
+          '150ms ease-out',
+          style({
+            opacity: 0,
+            height: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingRight: 0,
+            paddingLeft: 0,
+            'margin-bottom': 0
+          })
+        )
+      ])
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({
+              opacity: 0,
+              height: 0
+            }),
+            stagger(100, [animate('0.2s ease')])
+          ],
+          {
+            optional: true
+          }
+        )
+      ])
+    ])
+  ]
 })
 export class NoteHomeComponent implements OnInit {
   notes: Note[] = new Array<Note>();
